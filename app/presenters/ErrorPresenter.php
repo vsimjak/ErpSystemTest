a8e22661b39c110c5e10235021432b34fc7be4ac
vladis <vlado.simjak@gmail.com>
2016-07-09 07:41:39 +0200

    Default commit

---
+++ app/presenters/ErrorPresenter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\Responses;
+use Tracy\ILogger;
+
+
+class ErrorPresenter extends Nette\Object implements Nette\Application\IPresenter
+{
+	/** @var ILogger */
+	private $logger;
+
+
+	public function __construct(ILogger $logger)
+	{
+		$this->logger = $logger;
+	}
+
+
+	/**
+	 * @return Nette\Application\IResponse
+	 */
+	public function run(Nette\Application\Request $request)
+	{
+		$e = $request->getParameter('exception');
+
+		if ($e instanceof Nette\Application\BadRequestException) {
+			// $this->logger->log("HTTP code {$e->getCode()}: {$e->getMessage()} in {$e->getFile()}:{$e->getLine()}", 'access');
+			$module = substr($request->getPresenterName(), 0, strrpos($request->getPresenterName(), ':'));
+			return new Responses\ForwardResponse($request->setPresenterName($module . ($module ? ':' : '') . 'Error4xx'));
+		}
+
+		$this->logger->log($e, ILogger::EXCEPTION);
+		return new Responses\CallbackResponse(function () {
+			require __DIR__ . '/templates/Error/500.phtml';
+		});
+	}
+
+}
